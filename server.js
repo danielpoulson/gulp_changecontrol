23039e6e28f0da340661029886714657904ccca3
danielpoulson <danielpoulson@hotmail.com.au>
2017-02-07 14:53:32 +1100

    partway

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static('./dist'));
-
+app.use(require('./server/config/route'));
 
 app.get('*', function (req, res) {
     res.render('index.html');
